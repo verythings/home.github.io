6492a522956bb3f7c0f534bde32e539829fb79a0
verythings <78595476+verythings@users.noreply.github.com>
2022-02-24 13:58:37 +0900

    Add files via upload

--- assets/js/news.js
+++ assets/js/news.js
@@ -1,50 +1,52 @@
 $( function() {
   $( ".thumb-1" ).on( "click", function() {
-    $(".thumb-3").toggleClass("is-opened", 1000);
-    $(".thumb-2").toggleClass("is-opened", 1000);
-    $(this).toggleClass("is-opened", 1000);
+    $(".thumb-3").removeClass("is-opened", 1000);
+    $(".thumb-2").removeClass("is-opened", 1000);
+    $(this).addClass("is-opened", 1000);
 
     $(".thumb-3").insertAfter(this);
     $(".thumb-2").insertAfter(this);
-    $(".img-item2").insertAfter(".first");
+    $(".img-item").insertAfter(".first");
 
     $(".thumb-1 p").toggle();
-    $(".thumb-2 p").toggle();
-    $(".thumb-3 p").toggle();
+    $(".thumb-2 p").hide();
+    $(".thumb-3 p").hide();
 
   });
 });
 
 $( function() {
   $( ".thumb-2" ).on( "click", function() {
-    $(".thumb-3").toggleClass("is-opened", 1000);
-    $(".thumb-1").toggleClass("is-opened", 1000);
-    $(this).toggleClass("is-opened", 1000);
+    $(".thumb-3").removeClass("is-opened", 1000);
+    $(".thumb-1").removeClass("is-opened", 1000);
+    $(this).addClass("is-opened", 1000);
 
     $(".thumb-3").insertAfter(this);
     $(".thumb-1").insertAfter(this);
-    $(".img-item").insertAfter(".first");
+    $(".img-item2").insertAfter(".first");
 
-    $(".thumb-1 p").toggle();
+    $(".thumb-1 p").hide();
     $(".thumb-2 p").toggle();
-    $(".thumb-3 p").toggle();
+    $(".thumb-3 p").hide();
 
   });
 });
 
 $( function() {
   $( ".thumb-3" ).on( "click", function() {
-    $(".thumb-2").toggleClass("is-opened", 1000);
-    $(".thumb-1").toggleClass("is-opened", 1000);
-    $(this).toggleClass("is-opened", 1000);
+    $(".thumb-2").removeClass("is-opened", 1000);
+    $(".thumb-1").removeClass("is-opened", 1000);
+    $(this).addClass("is-opened", 1000);
 
     $(".thumb-2").insertAfter(this);
     $(".thumb-1").insertAfter(this);
     $(".img-item").insertAfter(".first");
 
-    $(".thumb-1 p").toggle();
-    $(".thumb-2 p").toggle();
+    $(".thumb-1 p").hide();
+    $(".thumb-2 p").hide();
     $(".thumb-3 p").toggle();
 
   });
 });
+
+// 
